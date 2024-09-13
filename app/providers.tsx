d5c54a497280b6214b6f603c711ec0e2048d4217
aalok28 <51107195+aalok28@users.noreply.github.com>
2024-09-13 18:27:32 +0530

    added auth provider

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,17 +2,21 @@
 
 import React from "react";
 import { ThemeProvider } from "./theme-provider";
+import { Toaster } from "@/components/ui/toaster";
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
-    <ThemeProvider
-      attribute="class"
-      defaultTheme="system"
-      enableSystem
-      disableTransitionOnChange
-    >
-      {children}
-    </ThemeProvider>
+    <>
+      <Toaster />
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        {children}
+      </ThemeProvider>
+    </>
   );
 };
 
