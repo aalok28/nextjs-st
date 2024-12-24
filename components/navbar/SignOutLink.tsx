04a88801869820f4176c049594c564800d634360
aalok28 <51107195+aalok28@users.noreply.github.com>
2024-12-24 23:24:35 +0530

    App Completed

--- components/navbar/SignOutLink.tsx
+++ components/navbar/SignOutLink.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { SignOutButton } from "@clerk/nextjs";
-import { useToast } from "../use-toast";
+import { useToast } from "@/components/ui/use-toast";
 import Link from "next/link";
 
 const SignOutLink = () => {
--- components/ui/toaster.tsx
+++ components/ui/toaster.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useToast } from "@/components/use-toast";
+import { useToast } from "@/components/ui/use-toast";
 import {
   Toast,
   ToastClose,
